d5cf548197ecb43376317b27454208e758646375
tuye0305 <tuye0305@gmail.com>
2019-02-08 18:20:53 -0800

    Add files via upload
    Updating the MergeTreeSimulation functions.

--- MergeTreeSimulation.R
+++ MergeTreeSimulation.R
@@ -1,22 +1,27 @@
 # We are trying to merging multiple trees into one single cohort definition
 library(MASS)
 
-########################### Helper Functions ##########################
-calculatePvalue <- function(df, metricIdx) {
-  treatmentIds <- which(df[[treatmentVar]] == 1)
-  controlIds <- which(df[[treatmentVar]] == 0)
-  y1 <- mean(df[,metricIdx][treatmentIds])
-  y0 <- mean(df[,metricIdx][controlIds])
-  n1 <- as.numeric(nrow(df[treatmentIds, ]))
-  n0 <- as.numeric(nrow(df[controlIds, ]))
-  varY1 <- var(df[,metricIdx][treatmentIds]) / n1
-  varY0 <- var(df[,metricIdx][controlIds]) / n0
-  delta <- y1 - y0
+rm(list = ls())      # Clear all variables
+graphics.off()    # Close graphics windows
 
-  varianceDelta <- varY1 + varY0
-  tStatsDelta <- delta / sqrt(varianceDelta)
-  pValueDelta <- 2 * pnorm(- abs(tStatsDelta))
-  pValueDelta
+########################### Helper Functions ##########################
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+calculatePvalue <- function(df, metric.idx, treatment.idx) {
+  treatment.ids <- which(df[[treatment.idx]]  == 1)
+  control.ids <- which(df[[treatment.idx]] == 0)
+  y.1 <- mean(df[, metric.idx][treatment.ids])
+  y.0 <- mean(df[, metric.idx][control.ids])
+  n.1 <- as.numeric(nrow(df[treatment.ids, ]))
+  n.0 <- as.numeric(nrow(df[control.ids, ]))
+  var.y1 <- var(df[, metric.idx][treatment.ids]) / n.1
+  var.y0 <- var(df[, metric.idx][control.ids]) / n.0
+  delta <- y.1 - y.0
+
+  variance.delta <- var.y1 + var.y0
+  tstats.delta <- delta / sqrt(variance.delta)
+  pValue.delta <- 2 * pnorm(- abs(tstats.delta))
+  pValue.delta
 }
 
 extractSet <- function(str){
@@ -26,143 +31,139 @@ extractSet <- function(str){
 }
 
 #take intersection of two Segments by evaluate the sets per feature
-intersectSegment <- function(S1, S2){
-  Segment_out <- list()
-  for (i in 1:length(S1)){
-    s1 <- S1[[i]]
-    s2 <- S2[[i]]
-    set1 <- extractSet(s1)
-    set2 <- extractSet(s2)
+intersectSegment <- function(S.1, S.2){
+  Segment.out <- list()
+  for (i in 1:length(S.1)){
+    set.1 <- extractSet(S.1[[i]])
+    set.2 <- extractSet(S.2[[i]])
 
-    set_intersect <- intersect(set1, set2)
-    if (is.null(set_intersect) || length(set_intersect) == 0) Segment_out[[i]] <- "NULL"
-    else Segment_out[[i]] <- paste(noquote(set_intersect), collapse=",")
+    set.intersect <- intersect(set.1, set.2)
+    if (is.null(set.intersect) || length(set.intersect) == 0) Segment.out[[i]] <- "NULL"
+    else Segment.out[[i]] <- paste(noquote(set.intersect), collapse=",")
   }
-  Segment_out
+  Segment.out
 }
 
-#Get (S1 - S2) by evaluate the sets per feature
-complementSegment <- function(S1, S2){
-  Segment_out <- list()
-  for (i in 1:length(S1)){
-    s1 <- S1[[i]]
-    s2 <- S2[[i]]
-    set1 <- extractSet(s1)
-    set2 <- extractSet(s2)
+#Get (S.1 - S.2) by evaluate the sets per feature
+complementSegment <- function(S.1, S.2){
+  Segment.out <- list()
+  for (i in 1:length(S.1)){
+    set.1 <- extractSet(S.1[[i]])
+    set.2 <- extractSet(S.2[[i]])
 
-    set_complement <- setdiff(set1, set2)
-    if (is.null(set_complement) || length(set_complement) == 0) Segment_out[[i]] <- "NULL"
-    else Segment_out[[i]] <- paste(noquote(set_complement), collapse=",")
+    set.complement <- setdiff(set.1, set.2)
+    if (is.null(set.complement) || length(set.complement) == 0) Segment.out[[i]] <- "NULL"
+    else Segment.out[[i]] <- paste(noquote(set.complement), collapse=",")
   }
-  Segment_out
+  Segment.out
 }
 
 buildSubtableSet <- function(Segments, Metrics){
-  subtableSet <- c()
-  for (segment in Segments){
+  subtable.set <- list()
+  for (j in 1:length(Segments)){
+    segment <- Segments[[j]]
     subtable <- Metrics
     for (i in 1: length(segment)){
-      s <- segment[[i]]
-      set1 <- extractSet(s)
-      subtable <- subtable[which(subtable[,i] %in% set1), ]
+      set.1 <- extractSet(segment[[i]])
+      subtable <- subtable[which(subtable[,i] %in% set.1), ]
     }
-    subtableSet <- append(subtableSet, subtable)
+    subtable.set[[j]] <- subtable
   }
+  subtable.set
 }
 
-isStatSig <- function(Segments, Metrics, metricsIdxList){
-  subtableSet <- buildSubtableSet(Segments, Metrics)
-  for (k in metricsIdxList){
+isStatSig <- function(Segments, Metrics, metrics.idx.list){
+  subtable.set <- buildSubtableSet(Segments, Metrics)
+  for (k in metrics.idx.list){
     flag <- TRUE
-    for (subtable in subtableSet){
-      print(nrow(subtable) == 0)
-      if (nrow(subtable) == 0 || calculatePvalue(subtable, k) > 0.5){
+    for (subtable in subtable.set){
+      if (nrow(subtable) == 0 || calculatePvalue(subtable, k, 6) > 0.1){
         flag <- FALSE
       }
     }
-    if (flag) flag
+    if (flag == TRUE) return(flag)
   }
   FALSE
 }
 
-equalSegment <- function(S1, S2){
+equalSegment <- function(S.1, S.2){
   flag <- TRUE
-  for (i in 1:length(S1)){
-    s1 <- S1[[i]]
-    s2 <- S2[[i]]
-    set1 <- extractSet(s1)
-    set2 <- extractSet(s2)
-    if (!setequal(set1,set2)) flag <- FALSE
+  for (i in 1:length(S.1)){
+    set.1 <- extractSet(S.1[[i]])
+    set.2 <- extractSet(S.2[[i]])
+    if (!setequal(set.1,set.2)) flag <- FALSE
   }
+  flag
 }
 
-replaceSegments <- function(C, Segments_remove, Segments_add){
-  C_out <- list()
+replaceSegments <- function(C, Segments.remove, Segments.add){
+  C.out <- list()
   cnt <- 1
   for (Seg in C){
-    if (!equalSegment(Seg, Segments_remove)){
-      C_out[[cnt]] <- Seg
+    if (!equalSegment(Seg, Segments.remove)){
+      C.out[[cnt]] <- Seg
       cnt <- cnt + 1
     }
     else{
-      C_out <- append(C_out, Segments_add)
-      cnt <- cnt + length(Segments_add)
+      for (seg in Segments.add){
+        C.out[[cnt]] <- unlist(seg)
+        cnt <- cnt + 1
+      }
     }
   }
-  C_out
+  C.out
 }
 
 mergeTrees <- function(Cohorts, Metrics){
-  C_out <- Cohorts[[1]]
+  C.out <- Cohorts[[1]]
   for (l in 2:length(Cohorts)){
-    C_base <- C_out
-    for (Segment1 in C_base){
-      for (Segment2 in Cohorts[[l]]){
-        Segment_int <- intersectSegment(Segment1, Segment2)
-        Segment_complement <- complementSegment(Segment1, Segment_int)
-        print(isStatSig(c(Segment_int, Segment_complement), Metrics, c(4,5)))
-        if (isStatSig(c(Segment_int, Segment_complement), Metrics, c(4,5))){
-          C_out <- replaceSegments(C_out, Segment1, c(Segment_int,Segment_complement))
-          Segment1 <- Segment_complement
+    C.base <- C.out
+    for (Segment.1 in C.base){
+      for (Segment.2 in Cohorts[[l]]){
+        Segment.int <- intersectSegment(Segment.1, Segment.2)
+        Segment.complement <- complementSegment(Segment.1, Segment.int)
+        if (isStatSig(list(Segment.int, Segment.complement), Metrics, c(4,5)) == TRUE){
+          C.out <- replaceSegments(C.out, Segment.1, list(Segment.int,Segment.complement))
+          Segment.1 <- Segment.complement
         }
       }
     }
   }
-  C_out
+  C.out
 }
 
 ########################### Preparing Example Input Data ##########################
 #Tree models in the forms of Cohorts
-C1 <- list(c("NULL", "0,1,2,3,4,5,6,7,8,9,10", "1,2,3"),
-           c("-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7", "NULL", "4,5"))
+C.1 <- list(c("0,1", "2,3,4,5,6,7,8,9,10", "1,2,3"),
+           c("-5,-4,-3,-2,-1,2,3,4,5,6,7", "0,1", "4,5"))
 
-C2 <- list(c("0,1", "2,3,4,5,6,7", "5"),
-           c("-5,-4,-3,-2,-1", "0,1", "1,2"),
-           c("2,3,4,5,6,7", "8,9,10","3,4"))
+C.2 <- list(c("0", "2,3,4,5,6", "1,2"),
+           c("-5,-4,-3,-2,-1", "0,1", "5"),
+           c("2,3,4,5,6,7", "7,8,9,10","3,4"))
 
-C3 <- list(c("-1,0,1,2,3,4", "5,6", "1,2"),
+C.3 <- list(c("-1,0,1,2,3,4", "5,6", "1,2"),
            c("-5,-4,-3-2", "0,1,2,3,4", "3"),
            c("5,6", "7,8,9", "4"),
            c("7", "10", "5"))
 
 
-Cohorts <- list(C1, C2, C3)
+Cohorts <- list(C.1, C.2, C.3)
 
 set.seed(1)
 #Metrics Data
-size <- 10000
-treatmentAssignment <- sample(0:1,size, replace = TRUE)
-feature1 <- sample(-5:7,size, replace = TRUE)
-feature2 <- sample(0:10,size, replace = TRUE)
-feature3 <- sample(1:5,size, replace = TRUE)
+size <- 100000
+treatment.assignment <- sample(0:1,size, replace = TRUE)
+feature.1 <- sample(-5:7,size, replace = TRUE)
+feature.2 <- sample(0:10,size, replace = TRUE)
+feature.3 <- sample(1:5,size, replace = TRUE)
 
 Metrics <- data.frame(
-  feature1 <- feature1,
-  feature2 <- feature2,
-  feature3 <- feature3,
-  metric1 <- feature1 * rnorm(size) + feature3 * rnorm(size) + treatmentAssignment * 0.1 + rnorm(size),
-  metric2 <- feature2 * rnorm(size) + treatmentAssignment * 5 + rnorm(size),
-  treatmentVar <- treatmentAssignment
+  feature.1 <- feature.1,
+  feature.2 <- feature.2,
+  feature.3 <- feature.3,
+  metric.1 <- feature.1 * rnorm(size) + feature.3 * rnorm(size) + treatment.assignment * 0.1 + rnorm(size),
+  metric.2 <- feature.2 * rnorm(size) + treatment.assignment * 5 + rnorm(size),
+  treatment.var <- treatment.assignment
 )
 
 
